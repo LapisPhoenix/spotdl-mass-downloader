8e6b7bc9d23090fcbe5fdfc58d60642d002dc992
Lapis <88260244+LapisPhoenix@users.noreply.github.com>
2023-04-30 15:15:13 -0700

    🐛Fixed formatting cuz im stupid

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import dotenv
 import spotipy
 import colorama
 from spotipy.oauth2 import SpotifyClientCredentials
-from concurrent.futures import ThreadPoolExecutor
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 def spotify_setup(file: str = ".env") -> spotipy.Spotify:
@@ -89,15 +88,28 @@ def download_album(album_link: str) -> None:
 
         download_path = f"{name} - {artist}"
 
+        # Check if album is already downloaded
+
+        if os.path.exists(os.path.join(os.getcwd(), download_path)):
+            print(f"{colorama.Fore.RED}{download_path} already downloaded.{colorama.Style.RESET_ALL}")
+            print(f"{colorama.Fore.YELLOW}Ignoring {download_path} as it has already been found within the directory. (From URL {album_link}) {colorama.Style.RESET_ALL}")
+
+            return
+
+
         # Download songs
+        print(f"{colorama.Fore.YELLOW}Downloading {download_path}{colorama.Style.RESET_ALL}")
         execute("spotdl", album_link, "--overwrite", "skip", "--output", download_path)
-        print(f"{colorama.Fore.GREEN}Downloaded {' ' * (50 - len(download_path))}{download_path} in {round(time.time() - start, 2)} seconds.{colorama.Style.RESET_ALL}")
+        print(f"{colorama.Fore.GREEN}Downloaded  {download_path} in {colorama.Style.BRIGHT}{round(time.time() - start, 2)}{colorama.Style.NORMAL} seconds.{colorama.Style.RESET_ALL}")
     except Exception as e:
+        # If the album link is not an album link
         if str(e) == "http status: 400, code:-1 - Unexpected Spotify URL type., reason: None":
             print(f"{colorama.Fore.RED}Invalid album link: {album_link}{colorama.Style.RESET_ALL}")
+            print(f"{colorama.Fore.RED}Failed to download in {colorama.Style.BRIGHT}{round(time.time() - start, 2)}{colorama.Style.NORMAL} seconds.{colorama.Style.RESET_ALL}")
         else:
             print(f"{colorama.Fore.RED}Failed to download album from {album_link}. Error: {e}{colorama.Style.RESET_ALL}")
-            print(f"{colorama.Fore.RED}Failed to download in {round(time.time() - start, 2)} seconds.{colorama.Style.RESET_ALL}")
+            print(f"{colorama.Fore.RED}Failed to download in {colorama.Style.BRIGHT}{round(time.time() - start, 2)}{colorama.Style.NORMAL} seconds.{colorama.Style.RESET_ALL}")
+
         return
 
 
@@ -120,7 +132,14 @@ def download_albums(urls: list[str]) -> None:
     ```
     """
     with ThreadPoolExecutor() as executor:
-        executor.map(lambda url: download_album(url), urls)
+        futures = [executor.submit(download_album, url) for url in urls]
+
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except Exception as e:
+                print(f"{colorama.Fore.RED}An error occurred: {e}{colorama.Style.RESET_ALL}")
+
 
 
 def main():
